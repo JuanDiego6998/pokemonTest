52007a19175fa6f51675552c4c5012b1af7514bb
Juan Diego Garzon <juandiegogarzon98@hotmail.com>
2022-02-24 16:28:25 -0500

    feat: unit tests app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -31,5 +31,33 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Listado de Pokemon');
+  });
+
+  it('should invoke onCreateNew on button click', fakeAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    spyOn(fixture.componentInstance, 'onCreateNew');
+    const button = compiled.querySelector('button');
+    button?.click();
+    tick();
+    expect(fixture.componentInstance.onCreateNew).toHaveBeenCalled();
+  }));
+
+  it('should set showForm to true when onCreateNew is called', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    fixture.componentInstance.onCreateNew();
+    expect(fixture.componentInstance.showForm).toBeTrue();
+  });
+
+  it('should render form when showForm', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    fixture.componentInstance.showForm = true;
+    fixture.detectChanges();
+    expect(compiled.querySelector('app-pokemon-form')).toBeTruthy();
   });
 });
