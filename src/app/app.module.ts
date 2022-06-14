609a34d8028b656082eb202aa70b8a898be68ecb
Juan Diego Garzon <jgarzona@nttdata.com>
2022-06-14 18:43:35 -0500

    second test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatTableModule } from '@angular/material/table';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatSelectModule } from '@angular/material/select';
 import { PokemonTableComponent } from './pokemon-table/pokemon-table.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
 
@@ -29,15 +21,7 @@ import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule,
-    FlexLayoutModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatTableModule,
-    MatIconModule,
-    MatSliderModule,
-    MatSelectModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
